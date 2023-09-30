4c7b81dc8986e2fdd8a8030b6fefd6f37efe9b42
Arafa Akter <arafaakter924@gmail.com>
2023-09-30 07:39:36 +0600

    see all button show in donation route

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -8,7 +8,8 @@ import Cart from "../Card/Cart";
 const Donation = () => {
 
     const [donation,setDonation] =useState([])
-    const [noFound,setNofound] = useState("")
+    const [noFound,setNofound] = useState("");
+    const[isShow,setIsShow] = useState(false)
 
     useEffect(()=>{
         const donationItems = JSON.parse(localStorage.getItem('donation'))
@@ -24,9 +25,18 @@ const Donation = () => {
     return (
         <div >
            {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
+
               <div className="grid grid-cols-2">
-                {donation.map(card=> <DonationCart key={card.id} card={card}></DonationCart>)}
+                {
+                    isShow ? donation.map((card)=>(
+                        <DonationCart key={card.id} card={card}></DonationCart>))
+
+                        : donation.slice(0,4).map((card)=>(
+                     <DonationCart key={card.id} card={card}></DonationCart>))
+                }
               </div>
+
+              <button onClick={()=> setIsShow(!isShow)} className="p-3 rounded bg-[#009444] text-white block mx-auto">See All</button>
 
             </div>}
         </div>
